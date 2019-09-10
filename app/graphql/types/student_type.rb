9ef491e804e096baa000cc07600ff46285b7cd16
OscarB <oscartic@gmail.com>
2019-09-10 00:34:10 -0300

    Set full_name in respond query

--- app/graphql/types/student_type.rb
+++ app/graphql/types/student_type.rb
@@ -2,10 +2,13 @@ module Types
   class StudentType < Types::BaseObject
     field :id, ID, null: false
     field :rut, String, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
+    field :full_name, String, null: false
     field :course, String, null: false
     field :number_list, Integer, null: false
     field :is_present, Boolean, null: false
+
+    def full_name
+      [object.first_name, object.last_name].compact.join(" ")
+    end
   end
 end
